e8db0b3f1afab2b7eaf0ee8f96e6c351f7b4c06f
TheUnchosen <longbui369@gmail.com>
2024-06-28 17:35:31 +0000

    123231

--- main.py
+++ main.py
@@ -7,6 +7,27 @@ def main():
     Code Your Program here
     ########################################
     """
+    if num1 >= num2 and num1 >= num3:
+        maxval = num1
+    elif num2 >= num1 and num2 >= num3:
+        maxval = num2
+    else:
+        maxval = num3
+
+    if num1 <= num2 and num1 <= num3:
+        minval = num1
+    elif num2 <= num1 and num2 <= num3:
+        minval = num2
+    else:
+        minval = num3
+
+    if (minval == num2 and maxval == num3) or (maxval == num2 and minval == num3):
+        median = num1
+    elif (minval == num1 and maxval == num3) or (maxval == num1 and minval == num3):
+        median = num2
+    else:
+        median = num3
+
 
     print(minval, median, maxval)
     ########################################
